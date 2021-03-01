db2c83cbacfce4b66f276f4acf0636f119db14c8
Universe-One <UniverseOne.Code@gmail.com>
2021-03-01 15:34:54 -0800

    Add canvas and fallback content

---
+++ main.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector("#game-canvas");
+
+// Check if canvas is supported
+if (canvas.getContext) {
+	const ctx = canvas.getContext("2d");
+}
+
+
+
+
+
+
+// Check to make sure canvas' display type is block
+//console.log(window.getComputedStyle(canvas).getPropertyValue("display"));
