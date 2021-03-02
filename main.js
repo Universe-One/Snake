5987a457ac4cff749ab50074465051c6e0864df3
Universe-One <UniverseOne.Code@gmail.com>
2021-03-01 21:38:21 -0800

    Implement ability to randomize food position

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Many of the calculations in this program are done using cellWidth and numCellsInRow, but can just as easy
+// be done using cellHeight and numCellsInColumn since they are symmetric.
+
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
 
@@ -10,16 +13,26 @@ const cellHeight = canvas.height / numCellsInColumn;
 const initPos = 60;
 let snakeLength = 4;
 
+const food = {
+	firstDraw: true,
+	xPosDefault: width - cellWidth - initPos,
+	yPosDefault: height / 2,
+	xPos: getRandom(),
+	yPos: getRandom()
+}
+
+
+
+// Run the program
 drawWalls();
 drawSnake();
+drawFood();
 drawFood();
 
 
 
 function drawWalls() {
 	ctx.fillStyle = "rgb(0, 0, 0)";
-	// for loop's condition expression and increment expression uses width, but could
-	// just as easily use height since they are symmetric.
 	for(let i = 0; i < width; i += cellWidth) {
 		ctx.fillRect(i, 0, cellWidth, cellHeight);
 		ctx.fillRect(0, i, cellWidth, cellHeight);
@@ -37,5 +50,17 @@ function drawSnake() {
 
 function drawFood() {
 	ctx.fillStyle = "rgb(255, 0, 0)"
-	ctx.fillRect(width - cellWidth - initPos, height / 2, cellWidth, cellHeight);
+
+	if (food.firstDraw) {
+		ctx.fillRect(food.xPosDefault, food.yPosDefault, cellWidth, cellHeight);
+		food.firstDraw = false;
+	} else {
+		ctx.fillRect(food.xPos, food.yPos, cellWidth, cellHeight);
+	}
+}
+
+function getRandom() {
+	// (cellWidth - 2) is 18 and is used because two of the cells in the 20 cell row belong to the walls.
+	// The play area is an 18 cell by 18 cell grid. The walls do not count as part of the play area.
+	return Math.floor(Math.random() * (numCellsInRow - 2)) * cellWidth + cellWidth;		
 }
